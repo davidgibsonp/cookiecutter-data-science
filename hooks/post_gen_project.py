6a859c0757eeaa1aa0eef3601ae2716998307689
David Gibson <dvdgbsn@gmail.com>
2020-06-21 19:27:58 -0400

    added git init hook

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,16 @@
+import webbrowser
+import os
+
+gitinit = input("git init? [y]es / [n]o: ") 
+if gitinit.lower() == 'y':
+	# gitinit
+	print("\ninitialized git repo")
+	os.system("git init")
+
+	# open browser to create repo
+	print("create remote Github repo")
+	webbrowser.open('https://github.com/new', new=2)
+
+	# add remote
+	remote = input("enter remote origin: ") 
+	os.system(f"git remote add origin {remote}")
